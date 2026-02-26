c94c5ba6cc24f9129d14d766d5eecf7747d8adde
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-02-25 23:44:41 -0300

    sing up and login

---
+++ app/src/main/java/com/example/paradise/CadastroActivity.java
@@ -0,0 +1,79 @@
+package com.example.paradise;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CadastroActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private EditText cadastroEmail, cadastroPassword;
+    private Button cadastroButton;
+    private TextView RedirecionarLoginText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        auth = FirebaseAuth.getInstance();
+        cadastroEmail = findViewById(R.id.cadastro_email);
+        cadastroPassword = findViewById(R.id.cadastro_password);
+        cadastroButton = findViewById(R.id.cadastro_button);
+        RedirecionarLoginText = findViewById(R.id.RedirecionarLoginText);
+
+        cadastroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = cadastroEmail.getText().toString().trim();
+                String pass = cadastroPassword.getText().toString().trim();
+
+                //validação de campos
+                if (user.isEmpty()){
+                    cadastroEmail.setError("Email não pode está vazio!");
+                    return;
+                }
+                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(user).matches()) {
+                    cadastroEmail.setError("Digite um e-mail válido!");
+                    return;
+                }
+                if (pass.isEmpty()){
+                    cadastroPassword.setError("Senha não pode está vazia!");
+                    return;
+                }
+                if (pass.length() < 6){
+                    cadastroPassword.setError("A senha tem que ter pelo menos 6 caracteres!");
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(CadastroActivity.this, "Cadastrado realizado com sucesso!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(CadastroActivity.this, LoginActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(CadastroActivity.this, "Erro: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        RedirecionarLoginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CadastroActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/paradise/LoginActivity.java
@@ -0,0 +1,110 @@
+package com.example.paradise;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+//Firebase Auth
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEmail, loginPassword;
+    private TextView RedirecionarCadastroText;
+    private Button loginButton;
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
+        RedirecionarCadastroText = findViewById(R.id.RedirecionarCadastroText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginEmail.getText().toString();
+                String pass = loginPassword.getText().toString();
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!pass.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        String userId = auth.getCurrentUser().getUid();
+                                        verificarCadastroBebe(userId);
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LoginActivity.this, "Erro ao realizar o Login!", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        loginPassword.setError("Campos vazios não são permitidos");
+                    }
+                } else if (email.isEmpty()) {
+                    loginEmail.setError("Email não pode está vazio.");
+                } else {
+                    loginEmail.setError("Por favor, insira um e-mail válido.");
+                }
+            }
+        });
+        RedirecionarCadastroText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, CadastroActivity.class));
+            }
+        });
+    }
+    private void verificarCadastroBebe(String userId) {
+        db.collection("bebes")
+                .whereEqualTo("paiId", userId) //vinculação direta para o SQL
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        //o pai já tem um bebê cadastrado
+                        if (!task.getResult().isEmpty()) {
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        } else {
+                            Intent intent = new Intent(LoginActivity.this, CadastroBebeActivity.class);
+                            intent.putExtra("USER_ID", userId);
+                            startActivity(intent);
+                        }
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Erro ao conectar com o banco.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
